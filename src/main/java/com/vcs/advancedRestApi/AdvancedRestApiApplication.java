035d9fc3a617c54b24be69e2ed6a7f15e179a50f
Vivek Chandra Sardar <chandravivek52@gmail.com>
2023-12-04 09:44:14 +0530

    initial-commit...

---
+++ src/main/java/com/vcs/advancedRestApi/AdvancedRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.vcs.advancedRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedRestApiApplication.class, args);
+	}
+
+}
